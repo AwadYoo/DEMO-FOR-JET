8e82dde41324d1456483891bed9eb8a7880ff0c3
awad_yoo <18755119046@163.com>
2019-05-16 16:14:55 +0800

    leetcode

--- MongodbDemo/src/main/java/com/demo/config/WebConfig.java
+++ MongodbDemo/src/main/java/com/demo/config/WebConfig.java
@@ -1,11 +1,5 @@
 package com.demo.config;
 
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
